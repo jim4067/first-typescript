fc49d31afd9a5bc11c7803517c038d99919d0f47
jim4067 <jimmyimpulse2@gmail.com>
2020-11-25 23:48:47 +0300

    feat: thorough checks for the user input

--- multiplier.ts
+++ multiplier.ts
@@ -1,8 +1,29 @@
-const multiplacator = (a: number, b: number, print_text: string) => {
+interface MultiplyValues {
+    value1: number,
+    value2: number,
+}
+
+const parseArguments = (args: Array<String>): MultiplyValues => {
+    if (args.length < 4) throw new Error("Not enough arguments");
+    if (args.length > 4) throw new Error("Too many arguments");
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided vales were not numbers');
+    }
+}
+
+const multiplicator = (a: number, b: number, print_text: String) => {
     console.log(print_text, a * b);
 }
 
-const a: number = Number(process.argv[2]);
-const b: number = Number(process.argv[3]);
-
-multiplacator(a, b, `multiplied ${a} and ${b} and the result turned out to be`)
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    multiplicator(value1, value2, `multiplied ${value1} and ${value2} and the result is `);
+} catch (err) {
+    console.log(err.message);
+}
