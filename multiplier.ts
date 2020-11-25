5f71d2d8c3ddd025718d02f4615733abcdcf5a02
jim4067 <jimmyimpulse2@gmail.com>
2020-11-25 23:31:14 +0300

    feat: using env vars to capture user input

--- multiplier.ts
+++ multiplier.ts
@@ -2,4 +2,7 @@ const multiplacator = (a: number, b: number, print_text: string) => {
     console.log(print_text, a * b);
 }
 
-multiplacator(3, 4, "multiplying 3 and 4, the result is: ");
+const a: number = Number(process.argv[2]);
+const b: number = Number(process.argv[3]);
+
+multiplacator(a, b, `multiplied ${a} and ${b} and the result turned out to be`)
