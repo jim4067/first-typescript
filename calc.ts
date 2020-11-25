36e81a2dc936e0e1148d20fda5de05d5122484f5
jim4067 <jimmyimpulse2@gmail.com>
2020-11-25 13:30:37 +0300

    feat: wrote a mini calculator app in typescript

---
+++ calc.ts
@@ -0,0 +1,23 @@
+type Operation = 'add' | 'multiply' | 'divide';
+
+type Result = number;
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+    switch (op) {
+        case 'add':
+            return a + b;
+        case 'multiply':
+            return a * b;
+        case 'divide':
+            if (b === 0) throw new Error("Can't divide by zero");
+            return a / b;
+        default:
+            throw new Error("operation is not add, mutliply or divide ");
+    }
+}
+
+try {
+    console.log(calculator(2, 4, "add"));
+} catch (err) {
+    console.log("Something went wrong", err.message)
+}
