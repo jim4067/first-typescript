3c8181c5e00584709ec578a5b1863b716cf7515a
jim4067 <jimmyimpulse2@gmail.com>
2020-11-29 20:41:17 +0300

    refactor: the final version of the main index file

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
 app.get('/ping', (_req, res) => {
-    res.send("this shit better work");
+    res.send("this shit better work this time");
 });
 
 const PORT = 3040;
 
 app.listen(PORT, () => {
-    console.log(`succesfully running the app on port ${PORT}`)
+    console.log(`successfully running the app on port ${PORT}`);
 });
+
+//importing causes the error that express.1 has a problem
